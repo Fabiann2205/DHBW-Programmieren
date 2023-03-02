206af0833bc8a4ca987411f552c177e5089b0e22
Fabian <spechtfabian@icloud.com>
2023-03-02 17:36:47 +0100

    first steps of vehicle class (race) 02032023

---
+++ src/de/dhbwka/java/exercise/classes/vehicles/Race.java
@@ -0,0 +1,7 @@
+package de.dhbwka.java.exercise.classes.vehicles;
+
+public class Race {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/de/dhbwka/java/exercise/classes/vehicles/Vehicle.java
@@ -0,0 +1,68 @@
+package de.dhbwka.java.exercise.classes.vehicles;
+
+public class Vehicle {
+    private int tires;
+    private double vMax, position, speed; //position in km
+
+    public Vehicle() {
+        this.tires = 0;
+        this.vMax = 0;
+        //this.position = 0;
+        //this.speed = 0;
+    }
+
+    public double getSpeed() {
+        return this.speed;
+    }
+
+    public void setSpeed(double speed) {
+        if (speed >= this.vMax) {
+            this.speed = this.vMax;
+        } else {
+            this.speed = speed;
+        }
+    }
+
+    public int getTires() {
+        return this.tires;
+    }
+
+    // public void setTires(int tires) {
+    //     this.tires = tires;
+    // }
+
+    public double getVMax() {
+        return this.vMax;
+    }
+
+    // public void setVMax(double vMax) {
+    //     this.vMax = vMax;
+    // }
+
+    public double getPosition() {
+        return this.position;
+    }
+
+    // public void setPosition(double position) {
+    //     this.position = position;
+    // }
+
+    public void drive(double minutes) {
+        if (speed != 0) {
+            this.position += minutes * (this.speed / 60);
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " tires='" + getTires() + "'" +
+            ", vMax='" + getVMax() + "'" +
+            ", position='" + getPosition() + "'" +
+            ", speed='" + getSpeed() + "'" +
+            "}";
+    }
+
+    
+}
