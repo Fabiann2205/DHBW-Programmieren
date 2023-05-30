8c7e55572c5791b642c897a3fcf934f9917c4b18
Fabian <spechtfabian@icloud.com>
2023-05-30 13:39:39 +0200

    übung bmi fertig gemacht 30.05.2023

--- src/de/dhbwka/java/exercise/ui/event/BMICalculator.java
+++ src/de/dhbwka/java/exercise/ui/event/BMICalculator.java
@@ -10,13 +10,19 @@ public class BMICalculator {
     JFrame frame;
     public BMICalculator() {
         frame = new JFrame();
-        JPanel panel = new JPanel();
-        // frame.setLayout(new BorderLayout(5, 5));
+        frame.setLayout(new BorderLayout(5, 5));
+        JPanel top = new JPanel();
+        top.setLayout(new GridLayout(4, 2, 2, 2));
 
-        JTextField gewicht = new JTextField("Weight");
-        panel.add(gewicht);
-        JTextField groesse = new JTextField("Height");
-        panel.add(groesse);
+
+        JLabel gewichtLabel = new JLabel("Weight [kg]: ");
+        JTextField gewicht = new JTextField("70");
+        top.add(gewichtLabel);
+        top.add(gewicht);
+        JLabel groesseLabel = new JLabel("Body height [m]: ");
+        JTextField groesse = new JTextField("1.80");
+        top.add(groesseLabel);
+        top.add(groesse);
 
 
         JRadioButton male = new JRadioButton("male", true);
@@ -24,24 +30,75 @@ public class BMICalculator {
         ButtonGroup gruppe = new ButtonGroup();
         gruppe.add(male);
         gruppe.add(female);
-        panel.add(male);
-        panel.add(female);
+        top.add(male);
+        top.add(female);
+
+        frame.add(top, BorderLayout.NORTH);
 
 
+        //Buttons für unten
+        JLabel bmiLabel = new JLabel("BMI: ");
+        JTextField bmi = new JTextField("21.604938271604937");
+        JLabel normalweightLabel = new JLabel(" ");
+        JTextField normalweight = new JTextField("Normal weight");
+
+        // Mitte Calculate Button
+        JPanel mid = new JPanel();
         JButton calculate = new JButton("Calculate");
-        panel.add(calculate);
+        calculate.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    bmi.setText(String.valueOf(Double.parseDouble(gewicht.getText()) / (Double.parseDouble(groesse.getText()) * Double.parseDouble(groesse.getText()))));
+                    if (male.isSelected()) {
+                        if(Double.parseDouble(bmi.getText()) < 20.0d) {
+                            normalweight.setText("Short weight");
+                        } else if(Double.parseDouble(bmi.getText()) <= 25.0d) {
+                            normalweight.setText("Normal weight");
+                        } else if(Double.parseDouble(bmi.getText()) <= 30.0d) {
+                            normalweight.setText("Overweight");
+                        } else if(Double.parseDouble(bmi.getText()) <= 40.0d) {
+                            normalweight.setText("Adiposity");
+                        } else if(Double.parseDouble(bmi.getText()) > 40.0d) {
+                            normalweight.setText("Massive Adiposity");
+                        }
+                    } else if (female.isSelected()) {
+                        if(Double.parseDouble(bmi.getText()) < 19.0d) {
+                            normalweight.setText("Short weight");
+                        } else if(Double.parseDouble(bmi.getText()) <= 24.0d) {
+                            normalweight.setText("Normal weight");
+                        } else if(Double.parseDouble(bmi.getText()) <= 30.0d) {
+                            normalweight.setText("Overweight");
+                        } else if(Double.parseDouble(bmi.getText()) <= 40.0d) {
+                            normalweight.setText("Adiposity");
+                        } else if(Double.parseDouble(bmi.getText()) > 40.0d) {
+                            normalweight.setText("Massive Adiposity");
+                        }
+                    }
+                } catch (Exception f) {
+                    gewicht.setText("Kein gültiger Wert!!!");
+                    groesse.setText("Kein gültiger Wert!!!");
+                    bmi.setText("Kein gültiger Wert!!!");
+                    normalweight.setText("Kein gültiger Wert!!!");
+                }
+            }
+        });
+        mid.add(calculate);
+        frame.add(mid, BorderLayout.CENTER);
 
 
-        JTextField bmi = new JTextField("BMI");
-        panel.add(bmi);
-        JTextField normalweight = new JTextField("Normal Weight");
-        panel.add(normalweight);
+        // bottom stuff
+        JPanel bot = new JPanel();
+        bot.setLayout(new GridLayout(2, 2, 2, 2));
+        bot.add(bmiLabel);
+        bot.add(bmi);
+        bot.add(normalweightLabel);
+        bot.add(normalweight);
+        frame.add(bot, BorderLayout.SOUTH);
         
-        frame.add(panel);
 
         frame.pack();
         frame.setTitle("Currency converter");
-        frame.setMinimumSize(new Dimension(100, 500));
+        frame.setMinimumSize(new Dimension(300, 250));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
