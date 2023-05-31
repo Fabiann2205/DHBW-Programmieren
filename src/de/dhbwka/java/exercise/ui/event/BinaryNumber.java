e70d0967f74b057c2fd187f14a1debb38ce6d8ee
Fabian <spechtfabian@icloud.com>
2023-05-31 13:59:20 +0200

    added numberguess game ui events 31.05.2023

---
+++ src/de/dhbwka/java/exercise/ui/event/BinaryNumber.java
@@ -0,0 +1,109 @@
+package de.dhbwka.java.exercise.ui.event;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+public class BinaryNumber implements ActionListener {
+    JFrame frame;
+    ImageIcon imgOff = new ImageIcon("off.png");
+    ImageIcon imgOn = new ImageIcon("on.png");
+    JToggleButton sieben;
+    JToggleButton sechs;
+    JToggleButton fuenf;
+    JToggleButton vier;
+    JToggleButton drei;
+    JToggleButton zwei;
+    JToggleButton eins;
+    JLabel ergebnis1;
+    JToggleButton nul;
+    public BinaryNumber() {
+        frame = new JFrame();
+        //Top Buttons
+        JPanel top = new JPanel();
+        top.setLayout(new GridLayout(1, 8, 2, 2));
+        sieben = new JToggleButton();
+        sieben.setIcon(imgOff);
+        sieben.setSelectedIcon(imgOn);
+        sieben.addActionListener(this);
+        top.add(sieben);
+        sechs = new JToggleButton();
+        sechs.setIcon(imgOff);
+        sechs.setSelectedIcon(imgOn);
+        sechs.addActionListener(this);
+        top.add(sechs);
+        fuenf = new JToggleButton();
+        fuenf.setIcon(imgOff);
+        fuenf.setSelectedIcon(imgOn);
+        fuenf.addActionListener(this);
+        top.add(fuenf);
+        vier = new JToggleButton();
+        vier.setIcon(imgOff);
+        vier.setSelectedIcon(imgOn);
+        vier.addActionListener(this);
+        top.add(vier);
+        drei = new JToggleButton();
+        drei.setIcon(imgOff);
+        drei.setSelectedIcon(imgOn);
+        drei.addActionListener(this);
+        top.add(drei);
+        zwei = new JToggleButton();
+        zwei.setIcon(imgOff);
+        zwei.setSelectedIcon(imgOn);
+        zwei.addActionListener(this);
+        top.add(zwei);
+        eins = new JToggleButton();
+        eins.setIcon(imgOff);
+        eins.setSelectedIcon(imgOn);
+        eins.addActionListener(this);
+        top.add(eins);
+        nul = new JToggleButton();
+        nul.setIcon(imgOff);
+        nul.setSelectedIcon(imgOn);
+        nul.addActionListener(this);
+        top.add(nul);
+        frame.add(top, BorderLayout.NORTH);
+        //Middle Labels
+        JPanel middle = new JPanel();
+        middle.setLayout(new GridLayout(1, 8, 2, 2));
+        JLabel label7 = new JLabel("2^7", SwingConstants.CENTER);
+        middle.add(label7);
+        JLabel label6 = new JLabel("2^6", SwingConstants.CENTER);
+        middle.add(label6);
+        JLabel label5 = new JLabel("2^5",SwingConstants.CENTER);
+        middle.add(label5);
+        JLabel label4 = new JLabel("2^4",SwingConstants.CENTER);
+        middle.add(label4);
+        JLabel label3 = new JLabel("2^3",SwingConstants.CENTER);
+        middle.add(label3);
+        JLabel label2 = new JLabel("2^2",SwingConstants.CENTER);
+        middle.add(label2);
+        JLabel label1 = new JLabel("2^1",SwingConstants.CENTER);
+        middle.add(label1);
+        JLabel label0 = new JLabel("2^0",SwingConstants.CENTER);
+        middle.add(label0);
+        frame.add(middle);
+        //down label
+        JPanel f = new JPanel();
+        ergebnis1 = new JLabel("0");
+        f.add(ergebnis1);
+        frame.add(f, BorderLayout.SOUTH);
+        // frame.pack();
+        frame.setTitle("Currency converter");
+        frame.setMinimumSize(new Dimension(500, 250));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent e){ 
+        JToggleButton[] namen = {nul, eins, zwei, drei, vier, fuenf, sechs, sieben};
+        int ergebnis = 0;
+        for(int i = 0; i<8 ; i++) {
+            if (namen[i].isSelected()) {
+                ergebnis += (Math.pow(2.0d, i));
+            }
+        }
+        ergebnis1.setText(String.valueOf(ergebnis));
+    }
+    public static void main(String[] args) {
+        new BinaryNumber();
+    }
+}
---
+++ src/de/dhbwka/java/exercise/ui/event/NumberGuess.java
@@ -0,0 +1,105 @@
+package de.dhbwka.java.exercise.ui.event;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.nio.channels.UnsupportedAddressTypeException;
+import java.util.Random;
+
+public class NumberGuess implements ActionListener {
+    JFrame frame;
+    int zumraten;
+    JButton newgame;
+    JButton ok;
+    JButton bestplayer;
+    JButton exit;
+    Random random = new Random();
+    JLabel ergebnis;
+    int versuche = 0;
+    JTextField playernamefield;
+    JTextField numberfield;
+    Boolean fertig = false;
+    public NumberGuess() {
+        frame = new JFrame();
+        frame.setLayout(new BorderLayout(5, 5));
+
+
+        //TOP
+        JPanel top = new JPanel();
+        top.setLayout(new GridLayout(2, 2, 2, 2));
+        JLabel playername = new JLabel("Player name: ");
+        playernamefield = new JTextField("Player name");
+        JLabel number = new JLabel("Enter number between 1 and 1000: ");
+        numberfield = new JTextField("Number");
+        top.add(playername);
+        top.add(playernamefield);
+        top.add(number);
+        top.add(numberfield);
+        frame.add(top, BorderLayout.NORTH);
+
+        //CENTER
+        JPanel center = new JPanel();
+        newgame = new JButton("New Game");
+        ok = new JButton("Ok");
+        bestplayer = new JButton("Best player");
+        exit = new JButton("Exit");
+        newgame.addActionListener(this);
+        ok.addActionListener(this);
+        bestplayer.addActionListener(this);
+        exit.addActionListener(this);
+        center.add(newgame);
+        center.add(ok);
+        center.add(bestplayer);
+        center.add(exit);
+        frame.add(center, BorderLayout.CENTER);
+
+        //BOTTOM
+        ergebnis = new JLabel("PRESS NEW GAME TO BEGIN!");
+        frame.add(ergebnis, BorderLayout.SOUTH);
+
+
+        frame.pack();
+        frame.setTitle("NumberGuess");
+        frame.setMinimumSize(new Dimension(300, 250));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    
+
+    public static void main(String[] args) {
+        new NumberGuess();
+    }
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == newgame) {
+            fertig = false;
+            versuche = 0;
+            zumraten = (random.nextInt(1000)+1);
+            ergebnis.setText("Attempt 1: bitte eingeben und ok klicken");
+            playernamefield.setEditable(false);
+        } else if (e.getSource() == ok) {
+            versuche++;
+            if (Integer.parseInt(numberfield.getText()) == zumraten && fertig == false) {
+                ergebnis.setText("Attempt " + versuche + ": Richtig Spieler " + playernamefield.getText());
+                fertig = true;
+                playernamefield.setEditable(true);
+            } else if (Integer.parseInt(numberfield.getText()) <= zumraten && fertig == false) {
+                ergebnis.setText("Attempt " + versuche + ": zu niedrig Spieler " + playernamefield.getText());
+            } else if (Integer.parseInt(numberfield.getText()) >= zumraten && fertig == false) {
+                ergebnis.setText("Attempt " + versuche + ": zu groß Spieler " + playernamefield.getText());
+            } else {
+                ergebnis.setText("Starte ein neues game!!!");
+            }
+        } else if (e.getSource() == bestplayer) {
+
+        } else if (e.getSource() == exit) {
+            System.exit(0);
+        } else {
+            throw new UnsupportedAddressTypeException();
+        }
+    }
+}
